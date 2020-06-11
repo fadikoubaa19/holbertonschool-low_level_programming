2417971f563f95b8f1e34b1c67497c5687146f54
fadikoubaa19 <2062@holbertonschool.com>
2020-06-11 22:56:09 +0100

    eeeee

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Last digits
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x;
+x = 48;
+while (x <= 57)
+{
+putchar(x);
+x++;
+}
+putchar('\n');
+return (0);
+}
