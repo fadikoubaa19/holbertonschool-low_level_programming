3cbcc75972dbf3d2003b1001161c4c80fa2ed897
fadikoubaa19 <2062@holbertonschool.com>
2020-06-18 12:50:42 +0100

    eee

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+  int a = 0;
+  while (a < 10)
+    {
+      _putchar(a + '0');
+      a++;
+    }
+  _putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * more_numbers - function that prints 10 times the numbers
+ * void
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int a, b;
+
+	for (b = 0; b < 10; b++)
+	{
+		for (a = 0; a <= 14; a++)
+		{
+			if (a >= 10)
+				_putchar('0' + (a / 10));
+			_putchar('0' + (a % 10));
+		}
+		_putchar('\n');
+	}
+}
