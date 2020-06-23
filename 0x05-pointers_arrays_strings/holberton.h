0d9f759ae484d17d9ec9c83eefe7c913d82ccf4b
fadikoubaa19 <2062@holbertonschool.com>
2020-06-23 23:53:30 +0100

    eee

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,11 +1,18 @@
-#ifndef _h_
-#define _h_
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
-int _putchar(char c);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
-#endif
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+
+#endif 
