c64f7a37e09370697dbf4d9a6684a00a4c0ee2c0
fadikoubaa19 <2062@holbertonschool.com>
2020-06-29 12:26:01 +0100

    aabab

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c) 
+		{
+			return (&s[i]) ;		
+		}
+		i++;
+	}
+	return (0);
+}
