42bdabe49f5b1a4209b2d887b9522511eafd2f3e
fadikoubaa19 <2062@holbertonschool.com>
2020-07-05 17:32:11 +0100

    eee

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,7 +6,11 @@
  *Return: always 0.
  */
 	int main(int argc, char *argv[])
+
 	{
-			printf("%s\n", argv[0]);
+		int i;
+
+		for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
 		return (0);
 }
