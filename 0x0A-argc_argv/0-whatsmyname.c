f4e2aeb3e8f93f6884ef30f40b6dbd1683d94713
fadikoubaa19 <2062@holbertonschool.com>
2020-07-05 17:22:32 +0100

    myname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,21 +7,6 @@
  */
 	int main(int argc, char *argv[])
 	{
-		int count;
-
-		count = 0;
-		argv[0] = "0";
-		if (argc - 1 > 0)
-		{
-			for (count = 0; count < argc - 1; count++)
-			{
-			}
-			printf("%d\n", count);
-		}
-		else if (count == 0)
-		{
 			printf("%s\n", argv[0]);
-		}
-
 		return (0);
 }
