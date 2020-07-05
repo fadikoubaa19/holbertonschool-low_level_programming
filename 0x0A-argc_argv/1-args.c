d70fc92be086f3d414ca107ba46c1a861b5d0667
fadikoubaa19 <2062@holbertonschool.com>
2020-07-05 17:55:15 +0100

    me

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
+/**
+ * main - check code please.
+ *@argc: var
+ *@argv: var.
+ *Return: always 0.
+ */
 int main(int argc, __attribute((unused))char *argv[])
 {
-    
-    printf("%d\n",argc-1);
-    return 0;
+printf("%d\n", argc - 1);
+return (0);
 }
