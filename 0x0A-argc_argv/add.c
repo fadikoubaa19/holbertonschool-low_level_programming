27099d2791578d7d69971d0d6385446050156aec
fadikoubaa19 <2062@holbertonschool.com>
2020-07-06 10:36:16 +0100

    calcualte please

---
+++ 0x0A-argc_argv/add.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ * main - argv display
+ * @argc: var
+ * @argv: str
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, j;
+	int sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (!(isdigit(argv[i][j])))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ * main - check my code.
+ * @argc: var.
+ * @argv: var.
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, j;
+	int sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (!(isdigit(argv[i][j])))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
