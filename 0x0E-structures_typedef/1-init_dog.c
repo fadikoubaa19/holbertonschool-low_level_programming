2bb5c37f527ef2dcb7154646a2c3b2a82bff74c3
fadikoubaa19 <2062@holbertonschool.com>
2020-07-13 21:31:34 +0000

    proc

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+ *init_dog - gilmour
+ *@a:var.
+ *@name: the  name
+ *@age: the age
+ *@owner:the owner
+ *Return: Always 0
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	while  (d == 0)
+ 	return;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
