fd5440a3f836a8d5b97555bb216af63937f28e3b
fadikoubaa19 <2062@holbertonschool.com>
2020-07-20 11:31:36 +0000

    hello

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,20 +8,20 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list m;
-	unsigned int i;
-	int sum;
+va_list m;
+unsigned int i;
+int sum;
 
-	if (n == 0)
-		return (0);
-	va_start(m, n);
-	sum = 0;
-	i = 0;
-	while (i < n)
-	{
-		sum += va_arg(m, int);
-		i++;
-	}
-	va_end(m);
-	return (sum);
+if (n == 0)
+return (0);
+va_start(m, n);
+sum = 0;
+i = 0;
+while (i < n)
+{
+sum += va_arg(m, int);
+i++;
+}
+va_end(m);
+return (sum);
 }
