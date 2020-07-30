64099082bb1f2c29afd6ab0490ff5a03f06f4e65
fadikoubaa19 <2062@holbertonschool.com>
2020-07-30 20:31:05 +0000

    fixed

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,5 +1,7 @@
-#include "lists.h"
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
 /**
  * free_list - frees all the nodes in a linked list
  * @head: pointer to the head of list_t linked list
