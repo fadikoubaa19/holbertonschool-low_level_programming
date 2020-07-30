36a2bcfd179b222a6793f53a7c076d197541f003
fadikoubaa19 <2062@holbertonschool.com>
2020-07-30 20:28:39 +0000

    fixed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void name(void) __attribute__ ((constructor));
+/**
+ * name - singly linked list
+ *
+ * for Holberton project
+ */
+void name(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
