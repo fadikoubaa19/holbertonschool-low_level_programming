e70a4034f028522e38bb5afef313c3e9960506ab
fadikoubaa19 <2062@holbertonschool.com>
2020-08-12 11:35:58 +0000

    fadi

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,30 +1,31 @@
-*#include "holberton.h"
+#include "holberton.h"
 /**
-* append_text_to_file - appends text
-* @filename:var
-* @text_content:var
-* Return: 0
-*/
+ * append_text_to_file - append text
+ * @filename: var
+ * @text_content:var
+ * Return:zero
+ **/
 int append_text_to_file(const char *filename, char *text_content)
 {
-int f = 0, b = 0, c = 0;
-if (!filename)
-{
-return (-1);
-}
-f = open(filename, O_WRONLY | O_APPEND);
-if (f == -1)
-{
-return (-1);
-}
-if (text_content)
-{
-while (text_content[b])
-b++;
-c = write(f, text_content, b);
-if (c == -1)
-return (-1);
-}
-close(f);
-return (1);
+	int f;
+	int wr = 0, len;
+
+	if (filename == NULL)
+		return (-1);
+	f = open(filename, O_RDWR | O_APPEND, 0600);
+
+	if (f == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len] != '\0'; len++)
+			;
+		wr = write(f, text_content, len);
+
+		if (wr == -1)
+			return (-1);
+	}
+	close(f);
+	return (1);
 }
