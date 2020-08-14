fb4672abd4325e90e853f0d3ff7e71e270fd806d
fadikoubaa19 <2062@holbertonschool.com>
2020-08-14 10:47:00 +0000

    mani

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,29 +1,34 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "holberton.h"
-
+#include <stdio.h>
 /**
- *binary_to_uint - check the code for Holberton School students.
- *@b: varrrr
+ * binary_to_uint - check the code for Holberton School students.
+ *@b:var
  * Return: Always 0.
  */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int k = 1;
-unsigned int i = 0;
-int c;
-unsigned int len;
-
-len = strlen(b);
-for (c = len - 1; c >= 0; c--)
-{
-if (b[c] != '0' && b[c] != '1')
+int a;
+unsigned int c = 0;
+if (!b)
 return (0);
-if (b[c] == '1')
+a = 0;
+while (b[a] != '\0')
 {
-i += k;
+if (b[a] != '0' && b[a] != '1')
+{
+return (0);
 }
-k *= 2;
+a++;
 }
-return (i);
+for (a = 0; b[a] != '\0'; a++)
+{
+c <<= 1;
+if (b[a] == '1')
+{
+c += 1;
+}
+}
+return (c);
 }
