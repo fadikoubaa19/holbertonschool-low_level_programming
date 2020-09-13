3d29637177c42672ed83dd924e9575392383548e
fadikoubaa19 <2062@holbertonschool.com>
2020-09-13 20:55:25 +0000

    fixing this shit

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * dlistint_len - for holbertonschool
+ * @h:var
+ *Return: Always EXIT_SUCCESS.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+int a = 0;
+while (h != NULL)
+{
+h = (*h).next;
+a++;
+}
+return (a);
+}
