f96b524ead425121683ec29cd2b0066426f7287c
fadikoubaa19 <2062@holbertonschool.com>
2020-09-13 21:11:39 +0000

    fixed bro

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -4,30 +4,30 @@
 #include "lists.h"
 
 /**
- * add_dnodeint - for Holberton School students.
+ * add_dnodeint - for Holberton .
  *@head :var
  *@n : var
  * Return: Always EXIT_SUCCESS.
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *m = malloc(sizeof(dlistint_t));
-	dlistint_t *a = malloc(sizeof(dlistint_t));
+	dlistint_t *end = malloc(sizeof(dlistint_t));
+	dlistint_t *tp = malloc(sizeof(dlistint_t));
 
-	if (m == NULL)
+	if (end == NULL)
 		return (NULL);
-	m->n = n;
-	m->prev = NULL;
-	m->next = NULL;
+	end->n = n;
+	end->prev = NULL;
+	end->next = NULL;
 	if (*head == NULL)
 	{
-		m->next = NULL;
-		*head = m;
+		end->next = NULL;
+		*head = end;
 		return (*head);
 	}
-	a = *head;
-	m->next = a;
-	m->prev = m;
-	*head = m;
-	return (m);
+	tp = *head;
+	end->next = tp;
+	tp->prev = end;
+	*head = end;
+	return (end);
 }
