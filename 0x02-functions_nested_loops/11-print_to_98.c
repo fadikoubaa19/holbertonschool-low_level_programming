b2cabb9e827352ece267987d421ac30655b82e0a
fadikoubaa19 <66315303+fadikoubaa19@users.noreply.github.com>
2021-01-08 20:41:48 +0100

    using for

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * print_to_98 - check the code for Holberton School students.
+ * print_to_98 - check the code for Holberton  students.
  *@n: fdfdfdfd
  * Return: Always 0.
  */
