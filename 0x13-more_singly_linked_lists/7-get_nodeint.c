0943cb8c3bdfdaee349ce5a47320e3e61e752288
fadikoubaa19 <66315303+fadikoubaa19@users.noreply.github.com>
2021-03-24 23:30:46 +0100

    nodeint

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 /**
- * get_nodeint_at_index - fafafaf.
+ * get_nodeint_at_index - for holbertonschool.
  * @head:var
  * @index: var
- * Return: fafa
+ * Return: returning head
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
